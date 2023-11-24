22db71d558fc7f529a141746576279c4abad113b
misakayu <misakayu@tencent.com>
2023-11-24 21:42:01 +0800

    feat: 路由控制器修改数据结构

--- src/components/route-stack-control/index.tsx
+++ src/components/route-stack-control/index.tsx
@@ -1,66 +1,81 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate, useNavigationType } from 'react-router-dom'
+
+// 路由链表节点
+interface RouteNode {
+  value: string
+  next: RouteNode | null
+  prev: RouteNode | null
+}
 
 const RouteStackControl = () => {
   const location = useLocation()
   const navigate = useNavigate()
   const navigationType = useNavigationType()
-  const [routeStack, setRouteStack] = useState([location.pathname])
-  const [currentRouteIndex, setCurrentRouteIndex] = useState(0)
+  // 初始化路由节点
+  const initialRouteNodeRef = useRef<RouteNode>({ value: location.pathname, next: null, prev: null })
+  // 当前的路由节点
+  const activeRouteNodeRef = useRef<RouteNode>(initialRouteNodeRef.current)
+  // 前进、后台的禁用判断
+  const [isBackDisabled, setIsBackDisabled] = useState(activeRouteNodeRef.current.prev == null)
+  const [isForwardDisabled, setIsForwardDisabled] = useState(activeRouteNodeRef.current.next == null)
 
   // 使用 useEffect 监听路由变化
   useEffect(() => {
-    console.log(routeStack)
     if (navigationType === 'PUSH') {
-      setRouteStack((prevRouteStack) => {
-        const newRouteStack = prevRouteStack.slice(0, prevRouteStack.length - currentRouteIndex)
-        if (newRouteStack[newRouteStack.length - 1] !== location.pathname) {
-          newRouteStack.push(location.pathname)
+      // 当PUSH的路由与当前路由节点值不同时，节点next一位
+      if (activeRouteNodeRef.current.value !== location.pathname) {
+        // 创建新节点
+        const newNode: RouteNode = {
+          value: location.pathname,
+          next: null,
+          prev: activeRouteNodeRef.current
         }
-        return newRouteStack
-      })
-      setCurrentRouteIndex(0)
+        // next一位
+        activeRouteNodeRef.current.next = newNode
+        // active指向新节点
+        activeRouteNodeRef.current = newNode
+      }
     } else if (navigationType === 'REPLACE') {
-      setRouteStack((prevRouteStack) => [
-        ...prevRouteStack.slice(0, prevRouteStack.length - currentRouteIndex - 1),
-        location.pathname
-      ])
-      setCurrentRouteIndex(0)
+      // 替换当前节点值
+      activeRouteNodeRef.current.value = location.pathname
     }
+    setIsBackDisabled(activeRouteNodeRef.current.prev == null)
+    setIsForwardDisabled(activeRouteNodeRef.current.next == null)
   }, [location, navigationType])
 
   // 处理后退按钮点击事件
   const handleGoBack = () => {
-    if (currentRouteIndex < routeStack.length - 1) {
-      setCurrentRouteIndex(currentRouteIndex + 1)
-      navigate(routeStack[routeStack.length - 2 - currentRouteIndex])
+    if (activeRouteNodeRef.current.prev != null) {
+      // prev一位
+      activeRouteNodeRef.current = activeRouteNodeRef.current.prev
+      // 跳转前一个路由
+      navigate(activeRouteNodeRef.current.value)
     }
   }
 
   // 处理前进按钮点击事件
   const handleGoForward = () => {
-    if (currentRouteIndex > 0) {
-      setCurrentRouteIndex(currentRouteIndex - 1)
-      navigate(routeStack[routeStack.length - currentRouteIndex])
+    if (activeRouteNodeRef.current.next != null) {
+      // next一位
+      activeRouteNodeRef.current = activeRouteNodeRef.current.next
+      // 跳转下一个路由
+      navigate(activeRouteNodeRef.current.value)
     }
   }
-
-  // 检查是否可以后退或前进
-  const canGoBack = currentRouteIndex < routeStack.length - 1
-  const canGoForward = currentRouteIndex > 0
 
   return (
     <div>
       <button
         className="iconfont icon-al w-7 h-7 leading-7 text-center rounded-full pr-0.5 text-ct enabled:hover:bg-[#e9e9e9] disabled:opacity-10"
         onClick={handleGoBack}
-        disabled={!canGoBack}
+        disabled={isBackDisabled}
         title="后退"
       ></button>
       <button
         className="iconfont icon-ar w-7 h-7 leading-7 text-center rounded-full pl-0.5 text-ct enabled:hover:bg-[#e9e9e9] disabled:opacity-10 ml-4"
         onClick={handleGoForward}
-        disabled={!canGoForward}
+        disabled={isForwardDisabled}
         title="前进"
       ></button>
     </div>
